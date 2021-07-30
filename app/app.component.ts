3e0ded3c7831752cdc3c3dd463da8f4750e1f90f
Jdrew001 <dtatkison@gmail.com>
2021-07-30 17:16:26 -0500

    2 files modified, 1 added, 1 deleted

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,43 +3,48 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
   /** Template reference to the canvas element */
   @ViewChild('canvasEl') canvasEl: ElementRef;
-  
+
   /** Canvas 2d context */
   private context: CanvasRenderingContext2D;
 
   constructor() {}
 
   ngAfterViewInit() {
-    this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
-  
-    this.draw();
+    this.context = (this.canvasEl
+      .nativeElement as HTMLCanvasElement).getContext('2d');
+
+    //this.draw();
+    this.drawImage();
   }
 
   /**
    * Draws something using the context we obtained earlier on
    */
   private draw() {
-    this.context.font = "30px Arial";
+    this.context.font = '30px Arial';
     this.context.textBaseline = 'middle';
     this.context.textAlign = 'center';
 
     const x = (this.canvasEl.nativeElement as HTMLCanvasElement).width / 2;
     const y = (this.canvasEl.nativeElement as HTMLCanvasElement).height / 2;
-    this.context.fillText("@realappie", x, y);
+    this.context.fillText('@realappie', x, y);
   }
 
   private drawImage() {
+    let that = this;
     const image = new Image();
-    image.src = '';
+    image.src = './dbuilding.png';
+    image.onload = function() {
+      that.context.drawImage(image, 0, 0);
+    };
   }
 
   open() {
-    window.open('https://github.com/realappie', '_blank')
+    window.open('https://github.com/realappie', '_blank');
   }
-
 }
