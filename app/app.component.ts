5efac44c8c8d3274a40d84c57e2f45248219372a
Jdrew001 <dtatkison@gmail.com>
2021-07-31 13:37:08 -0500

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,38 +1,109 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnInit
+} from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
   /** Template reference to the canvas element */
   @ViewChild('canvasEl') canvasEl: ElementRef;
+
+  startX = 0;
+  startY = 0;
+  mouseX = 0;
+  mouseY = 0;
+  bounds: DOMRect = null;
+  existingLines = [];
 
   /** Canvas 2d context */
   private context: CanvasRenderingContext2D;
 
+  private isMouseActivated = false;
+
+  get canvasElement(): HTMLCanvasElement {
+    return this.canvasEl.nativeElement as HTMLCanvasElement;
+  }
+
   constructor() {}
 
-  ngAfterViewInit() {
-    this.context = (this.canvasEl
-      .nativeElement as HTMLCanvasElement).getContext('2d');
+  ngOnInit() {}
 
+  ngAfterViewInit() {
+    this.context = this.canvasElement.getContext('2d');
+    this.bounds = this.canvasElement.getBoundingClientRect();
     //this.draw();
     this.drawImage();
   }
 
-  /**
-   * Draws something using the context we obtained earlier on
-   */
-  private draw() {
-    this.context.font = '30px Arial';
-    this.context.textBaseline = 'middle';
-    this.context.textAlign = 'center';
+  mouseup(e) {
+    if (e['button'] === 0) {
+      if (this.isMouseActivated) {
+        this.existingLines.push({
+          startX: this.startX,
+          startY: this.startY,
+          endX: this.mouseX,
+          endY: this.mouseY
+        });
 
-    const x = (this.canvasEl.nativeElement as HTMLCanvasElement).width / 2;
-    const y = (this.canvasEl.nativeElement as HTMLCanvasElement).height / 2;
-    this.context.fillText('@realappie', x, y);
+        this.isMouseActivated = false;
+      }
+
+      this.draw();
+    }
+  }
+
+  mousedown(e) {
+    if (e['button'] === 0) {
+      if (!this.isMouseActivated) {
+        this.startX = e['clientX'] - this.bounds.left;
+        this.startY = e['clientY'] - this.bounds.top;
+        this.isMouseActivated = true;
+      }
+
+      this.draw();
+    }
+  }
+
+  mousemove(e) {
+    this.mouseX = e.clientX - this.bounds.left;
+    this.mouseY = e.clientY - this.bounds.top;
+
+    if (this.isMouseActivated) {
+      this.draw();
+    }
+  }
+
+  private draw() {
+    this.context.fillStyle = '#333333';
+    this.context.fillRect(0, 0, canvasWidth, canvasHeight);
+    this.context.strokeStyle = 'black';
+    this.context.lineWidth = 2;
+    this.context.beginPath();
+
+    for (var i = 0; i < this.existingLines.length; ++i) {
+      var line = this.existingLines[i];
+      this.context.moveTo(line.startX, line.startY);
+      this.context.lineTo(line.endX, line.endY);
+    }
+
+    this.context.stroke();
+
+    if (this.isMouseActivated) {
+      this.context.strokeStyle = 'darkred';
+      this.context.lineWidth = 3;
+      this.context.beginPath();
+      this.context.moveTo(this.startX, this.startY);
+      this.context.lineTo(this.mouseX, this.mouseY);
+      this.context.stroke();
+    }
   }
 
   private drawImage() {
